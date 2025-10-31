8c5d080660a4c1a5aaf42fdd085530eab79ee59c
Nhat Duong <nhatvnet@gmail.com>
2025-10-31 19:04:48 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RMWD/' // 👈 rất quan trọng khi deploy lên GitHub Pages
+  base: '/RMWD/' 
 })
